a7a548ad4611ec4c0a112943329b39faa4ede53e
taramarie <taramarie@wifi-roaming-128-4-74-166.host.udel.edu>
2019-03-17 16:13:25 -0400

    ok

--- src/main/java/pkgGame/Sudoku.java
+++ src/main/java/pkgGame/Sudoku.java
@@ -35,7 +35,10 @@ public class Sudoku extends LatinSquare{
 	protected int[][] getPuzzle(){
 		return super.getLatinSquare();
 	}
-	
+	/*
+	 * A given region is passed in and a one-dimensional array of the regions
+	 * column or row is returned with its given values.
+	 */
 	protected int[] getRegion(int iRegionNbr) {
 		
 		int[][] sudokuSquare = getPuzzle();
@@ -57,7 +60,11 @@ public class Sudoku extends LatinSquare{
 			
 		return regionArray;
 	}
-		
+	/*
+	 * Takes in a row and column of a sudoku and returns what region you're in
+	 * based on the given values.
+	 */
+
 	protected int[] getRegion(int Col, int Row) {
 		return getRegion((int)(Math.floor(Row/iSqrtSize)*iSqrtSize+Math.floor(Col/iSqrtSize)));	
 	}
@@ -68,12 +75,57 @@ public class Sudoku extends LatinSquare{
 		return true;
 	}
 	
+	/*
+	 * 
+	 */
+	
 	protected boolean isPartialSudoku() {
 		setbIgnoreZero(true);
-		return false;
+		
+		boolean isPartialSudoku = true;
+		for (int i = 0; i < Sudoku.iSize; i++) {
+			if (hasDuplicates(getRow(i)))
+				return false;
+		}
+
+		for (int j = 0; j < Sudoku.iSize; j++) {
+			if (hasDuplicates(getColumn(j)))
+				return false;
+		}
+
+		for (int i = 1; i < Sudoku.iSize; i++) {
+
+			if (!hasAllValues(getRow(0), getRow(i))) {
+				return false;
+			}
+		}
+
+		for (int j = 1; j < Sudoku.iSize; j++) {
+
+			if (!hasAllValues(getColumn(0), getColumn(j))) {
+				return false;
+			}
+		}
+		
+		for (int k = 0; k < Sudoku.iSize; k++) {
+			if (hasDuplicates(getRegion(k))){
+				return false;
+			}
+		}
+		
+		for (int k = 1; k < Sudoku.iSize; k++) {
+			if (!hasAllValues(getRegion(0), getRegion(k))) {
+				return false;
+			}
+		}
+
+		return isPartialSudoku;
 	}
 	
 	@Override
+	/*
+	 * Tests to see if any element in a column or row is a duplicate.
+	 */
 	protected boolean hasDuplicates(int[] arr) {
 		boolean hasDuplicates = false;
 		if(isbIgnoreZero()) {
@@ -83,6 +135,9 @@ public class Sudoku extends LatinSquare{
 				return super.hasDuplicates(arr);
 		}
 	}
+	/*
+	 * Tests to see if a given value would work for a given column / row
+	 */
 	
 	protected boolean isValidValue(int iValue, int iRow, int iCol) {
 		if(iValue <= Sudoku.iSize && iValue > 0 && super.hasDuplicates(getRow(iRow)) && super.hasDuplicates(getColumn(iCol))) {
